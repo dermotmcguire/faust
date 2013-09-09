8eb860ffd82e701cfec4108e9958c83c00004102
Emma <e.chee@hotmail.com>
2013-09-09 14:53:53 +1000

    scene 3 animation and stick. animation is not smooth atm

--- perch/templates/layouts/global.scene3.php
+++ perch/templates/layouts/global.scene3.php
@@ -1,8 +1,14 @@
 <div id="scene3" class="scene part3  gradient-3"> <!-- Part 3 -->
-	<div class="body-gradient-flood centered alley">
+	<div class="body-gradient-flood centered alley" 
+			data-1-top			= "position: !relative;"
+			data-0-top			= "position: !fixed; top: 0%">
 		<div class="js-image scene-layer alley-master"
 			data-anchor-target	= "#scene3"
-			data-0-top 			= "display:block; transform: scale(3.0);"
+			data-0-top 			= "opacity: 0; display:!block; transform: scale(3.0); position: !relative; left: 10px; top: 10%"
+			data--1000-top		= "top: 0%; opacity: 1;";
+			data--4500-top		= "transform: scale(1.1);"
+			data--5000-top		= "top:-8%; transform: scale(1);"
+			data--5500-top		= "left:0px;" 
 			data-image-url		= "images/alley-master.jpg">
 			<img src="">
 		</div>
