acfaae5202a014778ed77554bb3ec7afd8d50cbf
Dermot McGuire <dermotmcguire@gmail.com>
2013-09-11 14:55:18 +1000

    Updated the sound and music handling - combining audio tracks.

--- js/sounds.js
+++ js/sounds.js
@@ -1,101 +1,78 @@
 $( document ).ready(function() {
-	$('#ex5-mute').on('click', function(){
-		Howler.mute(true);
-	});
-	$('#ex5-unmute').on('click', function(){
-		Howler.unmute(true);
-	});
+  //This space is for mute and unmute function
+  //function check on click whether the mute class is in place and performs appropriate function
+  $(".audio-button").click( function () {
+    //this toggles the mute class, performs the check logic
+    $(this).toggleClass("mute");
+    //after altering the class, run the appropriate howler function.
+    if ($(".audio-button").hasClass("mute") === true) {
+        Howler.mute();
+    }
+    else {
+        Howler.unmute();
+    }
+  });
 
-	$('#scene1').bind('inview', function (event, visible) {
-		if (visible) {
-			sound1.pause().fadeIn(0.5, 2000);
-			music1.pause().fadeIn(0.5, 1000);
-			music1.loop(false);
+
+
+  $('#scene1').bind('inview', function (event, visible) {
+    if (visible) {
+      scene1sounds.pause().fadeIn(0.5, 1000);
+			scene1sounds.loop(false);
 		} else {
-			sound1.fadeOut(0, 2000);
-			music1.fadeOut(0, 5000);
+			scene1sounds.fadeOut(0, 5000);
     	}
   	});
 
 	$('#scene2').bind('inview', function (event, visible) {
     	if (visible) {
-			sound2a.pause().fadeIn(0.5, 2000);
-			sound2b.pause().fadeIn(0.2, 2000);
-      		music2.pause().fadeIn(0.5, 2000);
-      		music2.loop(true);
+        scene2sounds.pause().fadeIn(0.5, 3000);
+  		  scene2sounds.loop(true);
     	} else {
-			sound2a.fadeOut(0, 2000);
-			sound2b.fadeOut(0, 2000);
-  	    	music2.fadeOut(0, 2000);
+        scene2sounds.fadeOut(0, 2000);
     	}
   	});
 
   $('#scene3').bind('inview', function (event, visible) {
-    	if (visible) {
-			sound3.pause().fadeIn(0.5, 2000);
-      		music3.pause().fadeIn(0.5, 2000);
-      		music3.loop(true);
-    	} else {
-			sound3.fadeOut(0, 2000);
-      		music3.fadeOut(0, 2000);
-    	}
+      if (visible) {
+        scene3sounds.pause().fadeIn(0.5, 3000);
+        scene3sounds.loop(true);
+      } else {
+        scene3sounds.fadeOut(0, 2000);
+      }
   	});
 
   $('#scene4').bind('inview', function (event, visible) {
-    if (visible) {
-			sound4.pause().fadeIn(0.5, 2000);
-      		music4.pause().fadeIn(0.5, 2000);
-      		music4.loop(true);
-		} else {
-			sound4.fadeOut(0, 2000);
-     		music4.fadeOut(0, 2000);
-   		}
+      if (visible) {
+        scene4sounds.pause().fadeIn(0.5, 3000);
+        scene4sounds.loop(true);
+      } else {
+        scene4sounds.fadeOut(0, 2000);
+      }
   	});
 
 });
 
-var sound1 = new Howl({
-  urls: ['sounds/scene-one-windy-city.mp3', 'sounds/scene-one-windy-city.ogg'],
+var scene1sounds = new Howl({
+  urls: ['sounds/scene1-combined.mp3', 'sounds/scene1-combined.ogg'],
   buffer: true,
   volume: 0.5
 });
-var sound2a = new Howl({
-  urls: ['sounds/scene-two-distant-city-sounds.mp3', 'sounds/scene-two-distant-city-sounds.ogg'],
-  buffer: true,
-  volume: 0.6
-});
-var sound2b = new Howl({
-  urls: ['sounds/scene-two-muffled-pub-sounds.mp3', 'sounds/scene-two-muffled-pub-sounds.ogg'],
-  buffer: true,
-  volume: 0.6
-});
-var sound3 = new Howl({
-  urls: ['sounds/scene-three-rainy-city-at-night.mp3', 'sounds/scene-three-rainy-city-at-night.ogg'],
+
+var scene2sounds = new Howl({
+  urls: ['sounds/scene2-combined.mp3', 'sounds/scene2-combined.ogg'],
   buffer: true,
   volume: 0.5
 });
-var sound4 = new Howl({
-  urls: ['sounds/scene-four-distant-city-sounds.mp3', 'sounds/scene-four-distant-city-sounds.ogg'],
+
+var scene3sounds = new Howl({
+  urls: ['sounds/scene3-combined.mp3', 'sounds/scene3-combined.ogg'],
   buffer: true,
-  volume: 0.8
+  volume: 0.5
 });
-var music1 = new Howl({
-  urls: ['sounds/scene-one-music-quiet.mp3', 'sounds/scene-one-music-quiet.ogg'],
+
+var scene4sounds = new Howl({
+  urls: ['sounds/scene4-combined.mp3', 'sounds/scene4-combined.ogg'],
   buffer: true,
-  volume: 0.1
-});
-var music2 = new Howl({
-  urls: ['sounds/scene-two-music-quiet.mp3', 'sounds/scene-two-music-quiet.ogg'],
-  buffer: true,
-  volume: 0.1
-});
-var music3 = new Howl({
-  urls: ['sounds/scene-three-music-quiet.mp3', 'sounds/scene-three-music-quiet.ogg'],
-  buffer: true,
-  volume: 0.1
-});
-var music4 = new Howl({
-  urls: ['sounds/scene-four-music-quiet.mp3', 'sounds/scene-four-music-quiet.ogg'],
-  buffer: true,
-  volume: 0.1
+  volume: 0.5
 });
