85caa50c2ad3381eefae2d2d85451f878e2106cd
Dermot McGuire <dermotmcguire@gmail.com>
2013-09-12 15:18:17 +1000

    Removed blurs from updated Scene 2 - too heavy to render.

--- perch/templates/layouts/global.scene2.php
+++ perch/templates/layouts/global.scene2.php
@@ -43,7 +43,6 @@
 		data-anchor-target	= "#scene2"
 		data-0-top			= "display:!block; opacity: 1.0; transform: translate3d(-5%, -30%, 0px) scale(1.55); filter: blur(3px);"
 		data-1000-bottom	= "filter: blur(1px); transform: translate3d(-12%, -32%, 0) scale(0.55);"
-		data-500-bottom		= "filter: blur(2px);"
 		data-image-url		= "images/room-master.png">
 		<span class="couch-salome-sprite"></span>
 		<img src="">
@@ -52,16 +51,15 @@
 		<div class="js-image scene-layer mephisto-master"
 		data-anchor-target 	= "#scene2"
 		data-0-top			= "display:!block; opacity: 1.0; transform: translate3d(15%, -30%, 0) scale(0.8);"
-		data-1000-bottom	= "transform: translate3d(-20%, -30%, 0) scale(0.6); filter: blur(0px);"
+		data-1000-bottom	= "transform: translate3d(-20%, -30%, 0) scale(0.6);"
 		data-image-url		= "images/room-mephisto-master-v2.png">
 		<img src="">
 		</div>
 
 		<div class="js-image scene-layer faust-master"
 		data-anchor-target 	= "#scene2"
-		data-0-top			= "display:!block; opacity: 1.0; filter: blur(5px); transform: translate3d(-10%, -30%, 0) scale(1.55);"
-		data-1000-bottom	= "filter: blur(5px); transform: translate3d(15%, -34%, 0) scale(0.6);"
-		data-500-bottom		= "filter: blur(0px);"
+		data-0-top			= "display:!block; opacity: 1.0; transform: translate3d(-10%, -30%, 0) scale(1.55);"
+		data-1000-bottom	= "transform: translate3d(15%, -34%, 0) scale(0.6);"
 		data-image-url		= "images/room-faust-master-v2.png">
 		<img src="">
 		</div>
